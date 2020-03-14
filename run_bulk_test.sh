b3cc941a75bf4089a681a8f8ab6f4ed8d774523e
Olga Mula <olga.mula@gmail.com>
2020-03-14 15:43:03 +0100

    Updated run scripts.

--- run_bulk_test.sh
+++ run_bulk_test.sh
@@ -3,7 +3,7 @@
 for T in 100 300 500 700 900 1100; do
 	for N in 10 20 30 40 50 60 70 80 100; do
 		for EPS in 1.0e-06 1.0e-08; do
-			qsub -v T=$T,N=$N,EPS=$EPS submit_run.pbs
+			qsub -v T=$T,N=$N,EPS=$EPS submit_run_brusselator.pbs
 		done
 	done
 done
