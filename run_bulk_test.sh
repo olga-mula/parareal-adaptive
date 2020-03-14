715c96114b8d29252a79728335cf05f002b73081
Olga Mula <olga.mula@gmail.com>
2020-03-14 12:07:51 +0100

    Renamed script files.

--- run_bulk_test.sh
+++ run_bulk_test.sh
@@ -2,8 +2,8 @@
 
 for T in 100 300 500 700 900 1100; do
 	for N in 10 20 30 40 50 60 70 80 100; do
-		for EPS in 1.0e-06 1.0e-07 1.0e-08 1.0e-09 1.0e-10; do
-			qsub -v T=$T,N=$N,EPS=$EPS script.pbs
+		for EPS in 1.0e-06 1.0e-08; do
+			qsub -v T=$T,N=$N,EPS=$EPS submit_run.pbs
 		done
 	done
 done
