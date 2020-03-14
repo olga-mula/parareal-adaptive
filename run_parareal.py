bf231fd038616d54f75f391c6dbe33455eec45c2
Olga Mula <olga.mula@gmail.com>
2020-03-14 15:29:07 +0100

    Update run scripts.

--- run_parareal.py
+++ run_parareal.py
@@ -52,7 +52,7 @@ T_formatted = '{:d}'.format(int(args.T))
 N_formatted = '{:d}'.format(args.N)
 eps_formatted = '{:.1e}'.format(args.eps)
 
-folder_name = args.ode_name + '/T_'+ T_formatted + '-N_'+ N_formatted + '-eps_'+ eps_formatted + '/'
+folder_name = args.ode_name + '/T_'+ T_formatted + '-N_'+ N_formatted + '-eps_'+ eps_formatted
 folder_name_sh = args.ode_name + '/T_'+T_formatted
 
 if not os.path.exists(folder_name):
@@ -76,9 +76,9 @@ compute_sh = args.compute_sh # Store eps-to-tol abacus
 p = Parareal_Algorithm(ode, ode.u0, [ti, tf], N, integrator_g=integrator_g, integrator_f=integrator_f, eps_g = eps_g, compute_sh=compute_sh)
 
 # Run classical parareal and store information on preformance
-pl, fl, gl, k_classic = p.run(eps, adaptive=False, balance_tasks=balance_tasks_cp)
+pl, fl, gl, k_classic = p.run(eps, adaptive=False, balance_tasks=balance_tasks_cp, kmax=15)
 summary_run(eps, p, pl, fl, gl, folder_name+'/non-adaptive/')
 
 # Run adaptive parareal and store information on preformance
-pl, fl, gl, k_adaptive = p.run(eps, adaptive=True, balance_tasks=balance_tasks_ap, kth = k_classic-1)
+pl, fl, gl, k_adaptive = p.run(eps, adaptive=True, balance_tasks=balance_tasks_ap, kth = k_classic-1, kmax=15)
 summary_run(eps, p, pl, fl, gl, folder_name+'/adaptive/')
