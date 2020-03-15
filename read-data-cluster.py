ff97345690aebacb0321d47a6f6b51c65cb92fcd
Olga Mula <olga.mula@gmail.com>
2020-03-15 16:56:17 +0100

    Improved data reading.

--- read-data-cluster.py
+++ read-data-cluster.py
@@ -1,57 +1,132 @@
-
 import argparse
 import numpy as np
+import re
+import os
+import pandas as pd
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 18})
+from matplotlib.ticker import MaxNLocator
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
 
 from ode import VDP, Brusselator, Oregonator
 
-# Dictionary of available odes
-ode_dict = {'VDP': VDP, 'Brusselator': Brusselator, 'Oregonator': Oregonator}
+def read_results(parent_dir):
+  """Extract results from folder of type parent_dir+'T_(\d+)-N_(\d+)-eps_(.*)' """
+  _, dirnames, _ = next(os.walk(parent_dir))
+  experiment_pattern = re.compile('T_(\d+)-N_(\d+)-eps_(.*)')
+  Ts, Ns, epss, cas, cnas, cseqs, spas, spnas, eas, enas = list(), list(), list(), list(), list(), list(), list(), list(), list(), list()
+  data = dict()
+  for d in dirnames:
+    m = experiment_pattern.match(d)
+    if m is None:
+      continue
+    T = int(m.group(1))
+    N = int(m.group(2))
+    eps = float(m.group(3))
+    
+    Ts.append(T)
+    Ns.append(N)
+    epss.append(eps)
 
-# Receive args
-# ------------
-parser = argparse.ArgumentParser()
-parser.add_argument(
-	'-ode', '--ode_name', default='Brusselator',
-	help='{VDP,Brusselator,Oregonator}')
-parser.add_argument(
-	'-fn', '--fn', default='prod-cluster', help='foldername')
-args = parser.parse_args()
-if args.ode_name not in ode_dict:
-	raise Exception('ODE type '+args.ode_name+' not supported')
+    # Read results
+    da  = np.load(parent_dir+'/'+d+'/adaptive/cost.npz')
+    dna = np.load(parent_dir+'/'+d+'/non-adaptive/cost.npz')
 
-# Read results
-# ------------
-tl = [100, 300, 500, 700, 900, 1100]
-nl = [10, 20, 30, 40, 50, 60, 70, 80, 100]
-epsl = [1.0e-06, 1.0e-07, 1.0e-08, 1.0e-09] # 1.e-10 too close to machine precision
+    ca = da['cost_f'].item()+da['cost_g'].item()
+    cna = dna['cost_f'].item()+dna['cost_g'].item()
+    cseq = da['cost_seq_fine'].item()
+    # Speed-up
+    spa = cseq / ca
+    spna = cseq / cna
+    # Efficiency
+    ea = cseq / (ca * N)
+    ena = cseq / (cna * N) 
 
-d = list()
+    # Add to lists
+    cas.append(ca)
+    cnas.append(cna)
+    cseqs.append(cseq)
+    spas.append(spa)
+    spnas.append(spna)
+    eas.append(ea)
+    enas.append(ena)
 
-for T in tl:
-	T_formatted = '{:d}'.format(int(T))
-	for N in nl:
-		N_formatted = '{:d}'.format(N)
-		for eps in epsl:
-			eps_formatted = '{:.1e}'.format(eps)
+  data = {'T': Ts, 'N': Ns, 'eps': epss, 'cseq': cseqs, 'ca': cas, 'cna': cnas, 'spa': spas, 'spna': spnas, 'ea': eas, 'ena': enas}
+  dataf = pd.DataFrame(data=data)
+  return dataf
 
-			fn_input = args.ode_name + '/'+ args.fn + '/T_'+ T_formatted + '-N_'+ N_formatted + '-eps_'+ eps_formatted + '/'
+def plot_performance(parent_dir, dataf):
+  """Make performance plots and save results in parent_dir"""
+  Ts = dataf['T'].unique()
+  epss = [1.e-6, 1.e-8]
+  # Color map
+  values = range(len(epss))
+  cm = plt.get_cmap('cool') 
+  cNorm  = colors.Normalize(vmin=0, vmax=values[-1])
+  scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=cm)
+  pltstyle = plt.plot
+  colorList = ['black', 'orange', 'blue', 'magenta']
 
-			da = np.load(fn_input+'adaptive/cost.npz')
-			dna = np.load(fn_input+'non-adaptive/cost.npz')
+  for i, T in enumerate(Ts):
 
-			# Cost
-			ca = da['cost_f'].item()+da['cost_g'].item()
-			cna = dna['cost_f'].item()+dna['cost_g'].item()
-			cseq = da['cost_seq_fine'].item()
+    # Speed-up as a function of N (parameter: eps)
+    plt.plot()
+    for j, eps in enumerate(epss):
+      colorVal = scalarMap.to_rgba(values[j])
+      colorVal = colorList[j]
+      sa = dataf[ (dataf['T']==T) &  (dataf['eps']==eps) ].sort_values(by=['N'])
+      Ns = sa['N'].to_numpy()
+      spas = sa['spa'].to_numpy()
+      spnas = sa['spna'].to_numpy()
+      pltstyle(Ns, spas, color=colorVal, marker='o', linestyle = '-', label='$\eta$='+str(eps)+' (Adaptive)')
+      pltstyle(Ns, spnas, color=colorVal, marker='x', linestyle = ':', label='$\eta$='+str(eps)+' (Non Adaptive)')
 
-			# Speed-up
-			spa = cseq / ca
-			spna = cseq / cna
-			
-			# Efficiency
-			ea = cseq / (ca * N)
-			ena = cseq / (cna * N) 
+    plt.ylim([0.1, 15])
+    plt.xlabel(r'Nb Processors')
+    plt.ylabel('Speed-up')
+    plt.title('T='+str(T))
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(parent_dir+'/speedup-T'+str(T)+'.pdf')
+    plt.close()
 
-			d.append({'T': T, 'N': N, 'eps': eps, 'cseq': cseq, 'ca': ca, 'cna': cna, 'spa': spa, 'spna': spna, 'ea': ea, 'ena': ena})
+    # Efficiency as a function of eps (parameter: N)
+    plt.plot()
+    for j, eps in enumerate(epss):
+      colorVal = scalarMap.to_rgba(values[j])
+      colorVal = colorList[j]
+      sa = dataf[ (dataf['T']==T) &  (dataf['eps']==eps) ].sort_values(by=['N'])
+      Ns = sa['N'].to_numpy()
+      eas = sa['ea'].to_numpy()
+      enas = sa['ena'].to_numpy()
+      pltstyle(Ns, eas, color=colorVal, marker='o', linestyle = '-', label='$\eta$='+str(eps)+' (Adaptive)')
+      pltstyle(Ns, enas, color=colorVal, marker='x', linestyle = ':', label='$\eta$='+str(eps)+' (Non Adaptive)')
 
-np.savez(args.ode_name+'/'+args.fn+'/data.npz', data=d)
+    plt.ylim([0.01, 0.6])
+    plt.xlabel(r'Nb Processors')
+    plt.ylabel('Efficiency')
+    plt.title('T='+str(T))
+    plt.legend()
+    plt.savefig(parent_dir+'/efficiency-T'+str(T)+'.pdf')
+    plt.close()
+
+if __name__ == "__main__":
+
+  # Dictionary of available odes
+  ode_dict = {'VDP': VDP, 'Brusselator': Brusselator, 'Oregonator': Oregonator}
+  # Receive args
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-ode', default='Brusselator', help='{VDP,Brusselator,Oregonator}')
+  parser.add_argument('-fn', default='prod-cluster',help='foldername')
+  args = parser.parse_args()
+  if args.ode not in ode_dict:
+    raise Exception('ODE type '+args.ode+' not supported')
+
+  # Read data from parent_dir containing experiments and make performance plots
+  parent_dir = args.ode + '/' + args.fn
+  dataf = read_results(parent_dir)
+  plot_performance(parent_dir, dataf)
--- run_bulk_test_vdp.sh
+++ run_bulk_test_vdp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for T in 10 20 30 40 50 100; do
+for T in 1000 2000 5000; do
 	for N in 10 20 30 40 50 60 70 80 100; do
 		for EPS in 1.0e-06 1.0e-08; do
 			qsub -v T=$T,N=$N,EPS=$EPS submit_run_vdp.pbs
