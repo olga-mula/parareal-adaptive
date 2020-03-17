85985ef63ec81f418d20e968561af6d8ffbc8cae
Olga Mula <olga.mula@gmail.com>
2020-03-17 15:29:32 +0100

    Scripts to run SEIR bulk tests.

--- run_bulk_test_seir.sh
+++ run_bulk_test_seir.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-for T in 150; do
-	for N in 10 20 30 40 50 60 70 80 100; do
-		for EPS in 1.0e-06 1.0e-08; do
-			qsub -v T=$T,N=$N,EPS=$EPS submit_run_vdp.pbs
+# for T in 100; do
+# 	for N in 10 20 30 40 50 60 70 80 100; do
+# 		for EPS in 1.0e-06 1.0e-08; do
+for T in 100; do
+	for N in 10; do
+		for EPS in 1.0e-06; do
+			qsub -v T=$T,N=$N,EPS=$EPS submit_run_seir.pbs
 		done
 	done
 done
